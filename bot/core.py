c030cb07e6de5bc3db1437a8dcb5a09ce209f757
Gab0 <gabriel_scf@hotmail.com>
2016-02-22 17:10:59 -0300

    Features added.

--- bot/core.py
+++ bot/core.py
@@ -405,7 +405,6 @@ def vestibular_questao():
 
 def retrieve_locality(Ilat,Ilng):
     mult = [-1,1]
-
     V = random.randrange(0,1)
     lat = Ilat + random.uniform(0,2)*mult[V]
     V = random.randrange(0,1)
@@ -413,31 +412,19 @@ def retrieve_locality(Ilat,Ilng):
 
     URL = "http://maps.googleapis.com/maps/api/geocode/json?latlng="+str(lat)+','+str(lng)+"&sensor=false"
     Content = urllib.request.urlopen(URL).read().decode('utf-8')
-
-
-
-    #print(Content)
-
     jsondata = json.loads(Content)
 
     Result = []
-
-    #logging.info("geography:"+str(jsondata["results"][1]["formatted_address"]))
-    region = None
-    city = None
     
     if len(jsondata["results"]) ==0:
-        #print('fail')
         return retrieve_locality(Ilat,Ilng)
+    
     result = jsondata["results"][0]
 
     if len(result["address_components"]) < 2:
-        #print('fail')
         return retrieve_locality(Ilat,Ilng)        
         
     for component in result["address_components"]:
-        #print(component)
-
         if 'administrative_area_level_2' in component["types"]:
             #print('>>> %s' % component['short_name'])
             Result.append(component['short_name'])
@@ -447,6 +434,5 @@ def retrieve_locality(Ilat,Ilng):
         if 'locality' in component["types"]:
             #print('>> %s' % component['short_name'])
             Result.append(component['short_name'])
-
 
     return Result
