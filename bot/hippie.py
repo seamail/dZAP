63339c6f2c79cb206d476cb91b42662bb2c1775a
Gab0 <gabriel_scf@hotmail.com>
2018-01-06 21:43:48 -0200

    .

--- bot/hippie.py
+++ bot/hippie.py
@@ -1,27 +1,38 @@
 #!/bin/python
 
-def retrieve_locality(Ilat,Ilng):
+import requests
+import json
+from random import randrange, uniform
+
+def retrieve_locality(originLatitude,originLongitude):
     mult = [-1,1]
     V = randrange(0,1)
-    lat = Ilat + uniform(0,2)*mult[V]
+    lat = originLatitude + uniform(0,2)*mult[V]
     V = randrange(0,1)
-    lng = Ilng + uniform(0,2)*mult[V]
+    lng = originLongitude + uniform(0,2)*mult[V]
 
-    URL = "http://maps.googleapis.com/maps/api/geocode/json?latlng="+str(lat)+','+str(lng)+"&sensor=false"
-    Content = urllib.request.urlopen(URL).read().decode('utf-8')
-    jsondata = json.loads(Content)
+    URL = "http://maps.googleapis.com/maps/api/geocode/json"
+
+    Parameters = {'latlng': "%f,%f" % (lat,lng),
+                  'sensor': 'false'}
+
+    Request = requests.get(URL, params=Parameters)
+
+    jsondata = json.loads(Request.text)
 
     Result = []
-    
+    print(jsondata)
     if len(jsondata["results"]) ==0:
-        return retrieve_locality(Ilat,Ilng)
-    
+        return retrieve_locality(originLatitude,originLongitude)
+
     result = jsondata["results"][0]
 
-    if len(result["address_components"]) < 2:
-        return retrieve_locality(Ilat,Ilng)        
-        
-    for component in result["address_components"]:
+    ac = result["address_components"]
+    print(ac)
+    if len(ac) < 2:
+        return retrieve_locality(originLatitude, originLongitude)
+
+    for component in ac:
         #if 'administrative_area_level_2' in component["types"]:
             #print('>>> %s' % component['short_name'])
             #Result.append(component['short_name'])
@@ -32,16 +43,16 @@ def retrieve_locality(Ilat,Ilng):
             #print('>> %s' % component['short_name'])
             Result.append(component['short_name'])
     if len(Result) < 2:
-        return retrieve_locality(Ilat,Ilng)
+        return retrieve_locality(originLatitude,originLongitude)
     Result.append([lat,lng])
 
-    middlelat = (Ilat - lat)/2
+    middlelat = (originLatitude - lat)/2
     middlelat = middlelat + lat
-    middlelng = (Ilng - lng)/2
+    middlelng = (originLongitude - lng)/2
     middlelng = middlelng + lng
 
     print("Coord Origin: %f,%f   Middle: %f,%f   Destination: %f,%f" %
-          (Ilat,Ilng,middlelat,middlelng,lat,lng))
+          (originLatitude,originLongitude,middlelat,middlelng,lat,lng))
     
     Result.append([middlelat,middlelng])
     
